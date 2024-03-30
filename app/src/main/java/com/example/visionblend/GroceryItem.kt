c5a81d32eb15dfd29d331a5bbcb6c05fbdd47c0e
Sahan Gunawardena <99317933+IT21250088@users.noreply.github.com>
2024-03-30 11:34:02 +0530

    Revert "Feature/it21250088"

--- app/src/main/java/com/example/visionblend/GroceryItem.kt
+++
@@ -1,6 +0,0 @@
-package com.example.visionblend
-
-data class GroceryItem(
-    val image: Int,
-    val title: String
-)
--- app/src/main/java/com/example/visionblend/cardview.kt
+++
@@ -1,7 +0,0 @@
-package com.example.visionblend
-
-import android.app.Activity
-
-class cardview : Activity() {
-
-}
