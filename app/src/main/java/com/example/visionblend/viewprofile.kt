aed687f39c21aa8417db469c8b025cde82a4623e
Binuka <binuka470@gmail.com>
2024-05-07 19:46:28 +0530

    update may 07

--- app/src/main/java/com/example/visionblend/viewprofile.kt
+++ app/src/main/java/com/example/visionblend/viewprofile.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
