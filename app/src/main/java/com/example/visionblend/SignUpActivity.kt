927901f802c66fc067672d2ff5b749e951702c97
Ashan <ashannirmal863@gmail.com>
2024-05-11 22:15:46 +0530

    magnification

--- app/src/main/java/com/example/visionblend/SignUpActivity.kt
+++ app/src/main/java/com/example/visionblend/SignUpActivity.kt
@@ -1,11 +1,9 @@
 package com.example.visionblend
 
-import android.content.Intent
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
